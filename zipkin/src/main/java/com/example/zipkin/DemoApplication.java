4ac628effebfe709e16d08735a3dfafc4aa73fce
liwei <liw@xinguangnet.com>
2018-03-08 19:08:58 +0800

    zipkin

---
+++ zipkin/src/main/java/com/example/zipkin/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+@SpringBootApplication
+@EnableZipkinStreamServer
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
